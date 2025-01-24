4332b52711a9ae94544fa8e415000d18f795e1f7
devkon <27301010+devk0n@users.noreply.github.com>
2025-01-24 06:29:34 +0100

    Update CMake version, simplify build setup, and remove Vulkan
    Upgraded the CMake version to 3.5 and changed the C++ standard to C++17. Removed Vulkan dependencies, related configurations, and Vulkan-specific code from the project. Simplified the `.gitignore` file to focus on essential exclusions.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,4 @@
-#include <vulkan/vulkan.h>
-#include <iostream>
-
 int main() {
-    VkInstance instance;
 
-    // Vulkan instance creation info
-    VkInstanceCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-
-    // Create Vulkan instance
-    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-        std::cerr << "Failed to create Vulkan instance!" << std::endl;
-        return -1;
-    }
-
-    std::cout << "Vulkan instance created successfully!" << std::endl;
-
-    // Clean up
-    vkDestroyInstance(instance, nullptr);
     return 0;
 }
